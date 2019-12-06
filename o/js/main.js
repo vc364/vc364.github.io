d401645c10ce23c84c5ee734759cb75a932bca90
vc364 <55567228+vc364@users.noreply.github.com>
2019-12-06 11:12:31 +0800

    更新
    2019 /12 /06

---
+++ o/js/main.js
@@ -0,0 +1,7 @@
+function menuToggle(){
+    var nav = document.getElementById('menu-overlay');
+    nav.classList.toggle('active');
+    var burger = document.getElementById('hamburger');
+    burger.classList.toggle('active');
+    }
+
